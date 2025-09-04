d75a8421fe78b156d4ee6100b75fe7ab7fe86efb
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-09-04 16:45:15 -0400

    (feat) append function
    -> appends new node at the end of list.

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 #!/usr/bin/node
 
+class LinkedList {
+	append(value) {
+		if (this.head == null) {
+			this.head = new Node(value);
+		} else {
+			let temp = this.head;
+			while (temp.nextNode != null) {
+				temp = temp.nextNode;
+			}
+			temp.nextNode = new Node(value);
+		}
+	}
+}
+
+class Node {
+	constructor(value = null, nextNode = null) {
+		this.value = value;
+		this.nextNode = nextNode;
+	}
+}
+const list = new LinkedList();
+
+list.append('hey');
+
+console.log(list);
